fa1f25f296a766e5a789c4dacd4798dea798b2c2
Paul Beusterien <paulbeusterien@google.com>
2020-09-13 10:30:05 -0700

    Fix platform build warning

--- Package.swift
+++ Package.swift
@@ -5,12 +5,6 @@ import PackageDescription
 
 let package = Package(
   name: "leveldb",
-    platforms: [
-        .macOS(.v10_10),
-        .iOS(.v8),
-        .tvOS(.v9)
-    ],
-
   products: [
     .library(
       name: "leveldb",
